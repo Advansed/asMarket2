cedc6d549093f7aff4303b6d7ece68f282ba72ec
Advansed <Atlasov.n.r@gmail.com>
2021-08-16 15:14:58 +0900

    commit

--- src/components/Infopage.tsx
+++ src/components/Infopage.tsx
@@ -1,13 +1,26 @@
-import { IonButton, IonCard, IonCardHeader, IonIcon, IonItem, IonList, IonText } from "@ionic/react"
+import { IonButton, IonCard, IonCardHeader, IonIcon, IonItem, IonList, IonText, IonCol,IonRow } from "@ionic/react"
 import { arrowBackOutline } from "ionicons/icons"
 import { Store } from "../pages/Store"
 import './Infopage.css'
 
 export function InfoPage1(props):JSX.Element {
     let elem = <>
-            <div className="info-header">
-                <a><b>Наши контакты</b></a>
-            </div>
+            <IonRow>
+            <IonCol size="3">
+                <IonIcon icon = { arrowBackOutline } 
+                    class= "back ml-1 mt-1 pr-btn2"
+                    onClick = {()=>{
+                    Store.dispatch({type: "route", route: "back"})
+                    }}
+                /> 
+            </IonCol>
+            <IonCol size="7">
+                <div className="pr-header">
+                    <IonText><h3><b>Контакты</b></h3></IonText>
+                    </div>
+            </IonCol>
+            </IonRow> 
+            
             <div className="info-content">
                 <p>
                     Вопросы, пожелания или жалобы Вы можете адресовать нам по следующим контактным данным:						
@@ -27,10 +40,21 @@ export function InfoPage1(props):JSX.Element {
 
 export function InfoPage2(props):JSX.Element {
     let elem = <>
-        
-            <div className="info-header">
-                <a><b>Помощь</b></a>
-            </div>
+            <IonRow>
+            <IonCol size="3">
+                <IonIcon icon = { arrowBackOutline } 
+                    class= "back ml-1 mt-1 pr-btn2"
+                    onClick = {()=>{
+                    Store.dispatch({type: "route", route: "back"})
+                    }}
+                /> 
+            </IonCol>
+            <IonCol size="7">
+                <div className="pr-header">
+                    <IonText><h3><b>Помощь</b></h3></IonText>
+                    </div>
+            </IonCol>
+            </IonRow>
             <details>
                 <summary>График работы</summary>
                 <div className="info-content2">
